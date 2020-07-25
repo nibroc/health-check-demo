c479a3a09f255be631bf773417cc4def45164c77
Corbin Hughes <corbin@corbinhughes.com>
2020-07-25 17:32:34 -0500

    Empty Spring Initializr project

---
+++ src/main/java/com/github/nibroc/healthcheckdemo/HealthCheckDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.nibroc.healthcheckdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nibroc/healthcheckdemo/HealthCheckDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nibroc.healthcheckdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
